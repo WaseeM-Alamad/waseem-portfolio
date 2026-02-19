07524e4307b036708e37a280b73ed66f277872f8
WaseeM-Alamad <2231341@std.hu.edu.jo>
2026-02-19 13:11:26 +0300

    animated plane and cloud version

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -17,11 +17,36 @@ const Home = () => {
     offset: ["start start", "end start"],
   });
 
-  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0.3]);
   const scale = useTransform(scrollYProgress, [0, 1], [1, 0.95]);
 
   return (
     <motion.section ref={ref} className="home" id="home">
+      <FlyingPlane />
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+            duration: 0.7,
+            ease: [0.55, 0, 0.15, 1],
+            opacity: { duration: 0.6, ease: "easeOut" },
+          }}
+        style={{ position: "absolute", insetInlineStart: "10%", top: "10%" }}
+      >
+        <svg
+          
+          width="141"
+          height="94"
+          viewBox="0 0 141 94"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M70.5 0C91.8849 0 109.715 15.2455 113.71 35.4553C128.927 36.5422 141 49.115 141 64.625C141 80.84 127.84 94 111.625 94H35.25C15.7744 94 0 78.2256 0 58.75C0 40.5669 13.7772 25.6147 31.4316 23.7053C38.8048 9.6349 53.5214 0 70.5 0Z"
+            fill="#F4F4F4"
+          />
+        </svg>
+      </motion.div>
       <motion.div
         style={{
           position: "relative",
@@ -29,33 +54,6 @@ const Home = () => {
           scale,
         }}
       >
-        <FlyingPlane/>
-        <svg
-          style={{ position: "absolute", left: "-26%", top: "0" }}
-          width="141"
-          height="94"
-          viewBox="0 0 141 94"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M70.5 0C91.8849 0 109.715 15.2455 113.71 35.4553C128.927 36.5422 141 49.115 141 64.625C141 80.84 127.84 94 111.625 94H35.25C15.7744 94 0 78.2256 0 58.75C0 40.5669 13.7772 25.6147 31.4316 23.7053C38.8048 9.6349 53.5214 0 70.5 0Z"
-            fill="#F4F4F4"
-          />
-        </svg>
-        <svg
-          style={{ position: "absolute", right: "-30%", top: "40%" }}
-          width="141"
-          height="94"
-          viewBox="0 0 141 94"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M70.5 0C91.8849 0 109.715 15.2455 113.71 35.4553C128.927 36.5422 141 49.115 141 64.625C141 80.84 127.84 94 111.625 94H35.25C15.7744 94 0 78.2256 0 58.75C0 40.5669 13.7772 25.6147 31.4316 23.7053C38.8048 9.6349 53.5214 0 70.5 0Z"
-            fill="#F4F4F4"
-          />
-        </svg>
         <motion.div
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -97,7 +95,7 @@ const Home = () => {
               ease: [0.55, 0, 0.15, 1],
             }}
             style={{
-              whiteSpace: "pre-line", 
+              whiteSpace: "pre-line",
               textAlign: "center",
               fontSize: "1.6rem",
               fontWeight: "600",
@@ -124,7 +122,6 @@ const Home = () => {
           </motion.div>
         </motion.div>
       </motion.div>
-
     </motion.section>
   );
 };
